f2bb600267f5258729f35dabdb3303c930c751ed
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-03-19 22:59:03 -0400

    now what

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,69 @@
 console.log("connected");
+
+const quizContent = [
+    {
+        number: 1,
+        question: "What is 1 + 1?",
+        answersArray: [
+            {position: "A", content: "2", correct: true},
+            {position: "B", content: "4", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 2,
+        question: "What is 2 + 2?",
+        answersArray: [
+            {position: "A", content: "4", correct: true},
+            {position: "B", content: "8", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 3,
+        question: "What is 4 + 4?",
+        answersArray: [
+            {position: "A", content: "8", correct: true},
+            {position: "B", content: "16", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 4,
+        question: "What is 8 + 8?",
+        answersArray: [
+            {position: "A", content: "16", correct: true},
+            {position: "B", content: "32", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 5,
+        question: "What is 16 + 16?",
+        answersArray: [
+            {position: "A", content: "32", correct: true},
+            {position: "B", content: "64", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 6,
+        question: "What is 32 + 32?",
+        answersArray: [
+            {position: "A", content: "64", correct: true},
+            {position: "B", content: "128", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },{
+        number: 7,
+        question: "What is 64 + 64?",
+        answersArray: [
+            {position: "A", content: "128", correct: true},
+            {position: "B", content: "256", correct: false},
+            {position: "C", content: "All of above", correct: false},
+            {position: "D", content: "None of above", correct: false},
+        ]
+    },
+];
+
