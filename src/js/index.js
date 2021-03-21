51e545692cc5f917f72aa81cccdad4584baf73ef
Jay0810 <jay.jiang23@sitechhs.com>
2021-03-20 22:44:15 -0400

    submit buttons

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,8 @@
 console.log("connected");
+
+//event listener for click on button
+const submitButton = document.getElementById("submit");
+
+submitButton.addEventListener("click", function (e) {
+  alert("Element has been clicked");
+});
