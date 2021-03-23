e8af7b368ad4087cd0ae740a8ea6f941f6d35da0
Jay0810 <jay.jiang23@sitechhs.com>
2021-03-22 22:21:12 -0400

    asdfsadfsa

--- src/js/index.js
+++ src/js/index.js
@@ -5,8 +5,4 @@ const startButton = document.getElementById("start");
 const addName = document.forms["add-name"];
 const value = addName.querySelector("input[type='text']").value;
 
-startButton.addEventListener("click", function (e) {
-  const name = document.createElement("li");
-  name.textContent = value;
-  li.appendChild(name);
-});
+startButton.addEventListener("click", function (e) {});
