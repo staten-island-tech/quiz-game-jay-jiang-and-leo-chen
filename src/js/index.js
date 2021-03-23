c290ed917f9013e6f3846ed83d17f773c4b362a3
LeoC05 <71610558+LeoC05@users.noreply.github.com>
2021-03-22 22:19:34 -0400

    package

--- src/js/index.js
+++ src/js/index.js
@@ -1,69 +1,161 @@
-console.log("connected");
+const score = 0;
 
-const quizContent = [
-    {
-        number: 1,
-        question: "What is 1 + 1?",
-        answersArray: [
-            {position: "A", content: "2", correct: true},
-            {position: "B", content: "4", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 2,
-        question: "What is 2 + 2?",
-        answersArray: [
-            {position: "A", content: "4", correct: true},
-            {position: "B", content: "8", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 3,
-        question: "What is 4 + 4?",
-        answersArray: [
-            {position: "A", content: "8", correct: true},
-            {position: "B", content: "16", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 4,
-        question: "What is 8 + 8?",
-        answersArray: [
-            {position: "A", content: "16", correct: true},
-            {position: "B", content: "32", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 5,
-        question: "What is 16 + 16?",
-        answersArray: [
-            {position: "A", content: "32", correct: true},
-            {position: "B", content: "64", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 6,
-        question: "What is 32 + 32?",
-        answersArray: [
-            {position: "A", content: "64", correct: true},
-            {position: "B", content: "128", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },{
-        number: 7,
-        question: "What is 64 + 64?",
-        answersArray: [
-            {position: "A", content: "128", correct: true},
-            {position: "B", content: "256", correct: false},
-            {position: "C", content: "All of above", correct: false},
-            {position: "D", content: "None of above", correct: false},
-        ]
-    },
-];
+const btn = document.querySelectorAll('button')
+btn.addEventListener("click", function() {
+    if (this.className === correct) {
+        score + 1;
+    } else {
+        score + 0;
+    }
+});
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Scratched forever
+/* console.log("connected");
+const one = {
+    question: "What is 1 + 1?",
+    contentA: "2",
+    contentB: "4",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+const two = {
+    question: "What is 2 + 2?",
+    contentA: "4", 
+    contentB: "8",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+const three = {
+    question: "What is 4 + 4?",
+    contentA: "8",
+    contentB: "16",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+const four = {
+    question: "What is 8 + 8?",
+    contentA: "16",
+    contentB: "32",
+    contentC: "All of above",
+    contentD: "None of above",
+}; 
+const five = {
+    question: "What is 16 + 16?",
+    contentA: "32",
+    contentB: "64",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+const six ={
+    question: "What is 32 + 32?",
+    contentA: "64",
+    contentB: "128",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+const seven = {
+    question: "What is 64 + 64?",
+    contentA: "128",
+    contentB: "256",
+    contentC: "All of above",
+    contentD: "None of above",
+};
+
+
+
+function questionize(number) {
+    const question = document.createElement('span');
+    document.body.append(question);
+    question.append(number);
+}
+questionize(one.question);
+questionize(two.question);
+questionize(three.question);
+questionize(four.question);
+questionize(five.question);
+questionize(six.question);
+questionize(seven.question);
+
+function answerize(number, a, b, c ,d) {
+    const answer = document.createElement('button');
+    document.querySelector('span').append(answer);
+    answer.append(a);
+    answer.append(b);
+    answer.append(c);
+    answer.append(d);
+}
+answerize(one.question, one.contentA, one.contentB, one.contentC, one.contentD);
+
+/* document.body.append(one.question, one.contentA, one.contentB, one.contentC, one.contentD);
+document.body.append(one.question);
+document.body.append(one.question);
+document.body.append(one.question);
+document.body.append(one.question);
+document.body.append(one.question); */
+ 
